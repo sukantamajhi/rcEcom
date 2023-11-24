6bfc464c8db4bf693f2bcf937cbed34a5446ba94
rajdwipDey <rajdwip.dey@underscoretec.com>
2023-11-24 17:49:21 +0530

    update pages and folder

--- src/components/auth/login/Login.tsx
+++ src/components/auth/login/Login.tsx
@@ -1,8 +1,28 @@
+import Image from 'next/image'
 import React from 'react'
+import logo from '../../../images/dressCodeLogo.svg'
+import TextFieldInput from '../../../common/inputField/TextFieldInput'
+import { Button } from '@mui/material'
+import { useRouter } from 'next/router'
 
 const Login = () => {
+
+  const router = useRouter()
+
   return (
-    <div>Login</div>
+    <div className={`flex justify-center items-center w-full h-screen`}>
+      <div className={`flex flex-col justify-center items-center loginShadow w-[50%] rounded-md px-[4%] pt-[3%] pb-[4%]`}>
+        <Image src={logo} alt='logo_break' height={100} width={100} className={`cursor-pointer`} onClick={()=> router.push(`/`)}/>
+        <div className={`w-full flex flex-col gap-4`}>
+          <TextFieldInput label={`Email`} />
+          <TextFieldInput label={`Passwoard`} />
+          <div className={`flex flex-row gap-8 mt-6`}>
+            <Button className={`w-full bg-blue-700`} type='submit' variant="contained">Login</Button>
+            <Button className={`w-full bg-blue-400`} variant="contained">Signup</Button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,28 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Header from '../common/Header'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Header from "../common/Header";
+import Login from "../components/auth/login/Login";
+import { useState, useEffect } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <div>
-      <Header />
-    </div>
-  )
+    const [loginModal, setLoginModal] = useState(false);
+
+    useEffect(() => {
+        setLoginModal(true);
+    }, [loginModal]);
+
+    return (
+        <div className={``}>
+            <div className={`sticky top-0`}>
+                <Header />
+            </div>
+            {loginModal && (
+                <div className={`absolute w-full`}>
+                    <Login />
+                </div>
+            )}
+        </div>
+    );
 }
